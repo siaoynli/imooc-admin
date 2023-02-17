2f0d2aa5f2ed8277eb44c3d13c036d1f545116f6
siaoynli <siaoynli@126.com>
2023-02-17 12:01:36 +0800

    fix(custom): ok

--- .cz-config.js
+++ .cz-config.js
@@ -25,13 +25,14 @@ module.exports = {
   // 消息步骤
   messages: {
     type: '请选择提交类型:',
-    // scope: '请输入修改范围 (可选):',
+    customScope: '请输入修改范围 (可选):',
     subject: '请简要描述提交(必填):',
     body: '请输入详细描述(可选):',
     breaking: '列出任何 BREAKING CHANGES (可选):',
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
+  allowCustomScopes: true,
   // 跳过问题
   // skipQuestions: ['body', 'footer'],
   // subject文字长度默认是72
