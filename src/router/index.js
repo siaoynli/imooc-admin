cdef81a96fbd1ebe7a007d0fcae040f1653bc5c8
siaoynli <siaoynli@126.com>
2023-02-17 11:07:09 +0800

    feat(custom): 添加登陆界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+const publicRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/LoginIndex.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: publicRoutes
 })
 
 export default router
